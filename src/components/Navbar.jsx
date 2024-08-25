6552cfb90ec3107d168fa3ff194853e94736bf6c
Sam Kikoma <kikomasamson@gmail.com>
2024-08-25 18:16:12 +0200

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { CgMenu, CgClose } from "react-icons/cg";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+// Disable body scroll when the mobile menu is open
+  useEffect(() => {
+    if (nav) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [nav]);
+  
+  const handleNav = () => {
+    setNav(!nav);
+    console.log(nav);
+  };
+
+  return (
+    <div className='w-full h-[90px] text-white md:border-white md:border-b-[1px]'>
+      <div className='max-w-[1240px] p-4 mx-auto flex justify-between items-center h-full '>
+        <div>
+          <h1 className="tracking-widest uppercase md:text-xl">Hawkeye photography</h1>
+        </div>
+        <div className="hidden md:flex">
+          <ul className="flex space-x-4 cursor-pointer">
+            <li className="hover:underline">About</li>
+            <li className="hover:underline">Pricing</li>
+            <li className="hover:underline">Contact</li>
+          </ul>
+        </div>
+        <div className="md:hidden" onClick={handleNav}>
+          {nav ? <CgClose size={30}/> : <CgMenu size={30}/>}
+        </div>
+        {/* Mobile menu */}
+        <div className={
+          nav ? "w-full h-[100vh] absolute top-[120px] left-0 md:hidden text-2xl flex justify-center" 
+          : "absolute left-[-100%]"
+        }>
+          <ul className="flex flex-col items-center gap-4 mt-20 text-3xl">
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
