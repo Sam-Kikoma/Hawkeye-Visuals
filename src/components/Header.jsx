31e0fff62287e01b8488cf12431656afd8ba1606
Sam Kikoma <kikomasamson@gmail.com>
2024-08-25 20:40:14 +0200

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <h1 className="text-white text-center uppercase text-[4rem] md:text-[16rem] tracking-wide font-Playfair-Display font-normal">Hawkeye</h1>
+        </div>
+        <div className="">
+            <img className="max-w-[100%] h-auto" src="https://images.unsplash.com/photo-1724497508903-0ed09e9bec2d?q=80&w=2042&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
